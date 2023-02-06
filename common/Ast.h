8ba137cea17d769c9dfb05ba6c8b560d6094ccab
YfsBox <1027342142@qq.com>
2023-02-06 17:27:26 +0800

    init

---
+++ common/Ast.h
@@ -0,0 +1,8 @@
+//
+// Created by 杨丰硕 on 2023/2/6.
+//
+
+#ifndef YFSCC_AST_H
+#define YFSCC_AST_H
+
+#endif //YFSCC_AST_H
---
+++ common/AstVisitor.h
@@ -0,0 +1,8 @@
+//
+// Created by 杨丰硕 on 2023/2/6.
+//
+
+#ifndef YFSCC_ASTVISITOR_H
+#define YFSCC_ASTVISITOR_H
+
+#endif //YFSCC_ASTVISITOR_H
